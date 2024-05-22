8f3721b4435252efe6f512cd968265bc6829dd06
MegaChubbzz <lockwood.ethan@yahoo.com>
2024-05-21 22:09:10 -0400

    Made a simple blackjack game

---
+++ Blackjack_Project/blackjack.py
@@ -0,0 +1,70 @@
+import random
+import sys
+import os
+from blackjack_art import logo
+
+def hit_me():
+    new_card = random.choice(cards)
+    return new_card
+def deal():
+    while  len(dealer_hand) <= 1:
+        new_card = hit_me()
+        dealer_hand.append(new_card)
+    while  len(player_hand) <= 1:
+        new_card = hit_me()
+        player_hand.append(new_card)
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+dealer_hand = []
+player_hand = []
+print(logo)
+want_to_play = input("Do you want to play a game of blackjack? Type 'y' or 'n': ")
+os.system("cls")
+print(logo)
+if want_to_play == 'y':
+    deal()
+else:
+    print("Probably better for your wallet that way.")
+
+while sum(dealer_hand) <= 17:
+    new_card = hit_me()
+    dealer_hand.append(new_card)
+    if sum(dealer_hand) > 21 and 11 in dealer_hand:
+        ace_index = dealer_hand.index(11)
+        dealer_hand[ace_index] = 1
+
+print(f'    Your cards: {player_hand}, current score: {sum(player_hand)}')
+print(f'    Computer\'s first card: {dealer_hand[0]}')
+another_card = input("Type 'y' to get another card, type 'n' to pass: ")
+while another_card == 'y':
+    new_card = hit_me()
+    player_hand.append(new_card)
+    
+    if sum(player_hand) > 21 and 11 in player_hand:
+        ace_index = player_hand.index(11)
+        player_hand[ace_index] = 1
+    else:
+        print(f'You went over 21, you bust!')
+        print(f'Player hand: {player_hand} Player total: {sum(player_hand)}')
+        print(f'Computer hand: {dealer_hand} Computer total: {sum(dealer_hand)}')
+        os.execv(sys.executable, [sys.executable] + sys.argv)
+            
+    print(f'    Your cards: {player_hand}, current score: {sum(player_hand)}')
+    print(f'    Computer\'s first card: {dealer_hand[0]}')
+    another_card = input("Type 'y' to get another card, type 'n' to pass: ")
+
+if sum(dealer_hand) > sum(player_hand) and sum(dealer_hand) < 21:
+    print("Computer wins!")
+    print(f'Computer hand: {dealer_hand} Computer total: {sum(dealer_hand)}')
+    print(f'Player hand: {player_hand} Player total: {sum(player_hand)}')
+    os.execv(sys.executable, [sys.executable] + sys.argv)
+elif sum(dealer_hand) == sum(player_hand):
+    print("It's a draw!")
+    print(f'Player hand: {player_hand} Player total: {sum(player_hand)}')
+    print(f'Computer hand: {dealer_hand} Computer total: {sum(dealer_hand)}')
+    os.execv(sys.executable, [sys.executable] + sys.argv)
+else:
+    print("Player wins!")
+    print(f'Player hand: {player_hand} Player total: {sum(player_hand)}')
+    print(f'Computer hand: {dealer_hand} Computer total: {sum(dealer_hand)}')
+    os.execv(sys.executable, [sys.executable] + sys.argv)
