048bc6ee38f9d02f323a8fd6b3a40d0967dd4f42
MegaChubbzz <lockwood.ethan@yahoo.com>
2024-05-26 00:02:48 -0400

    Small changes

--- Coffee_Machine_Project/coffee_machine.py
+++ Coffee_Machine_Project/coffee_machine.py
@@ -1,4 +1,4 @@
-'''Imported sys module to '''
+'''Imported sys module to turn off coffee machine when requested'''
 import sys
 
 MONEY = 0.0
@@ -6,7 +6,7 @@ PENNY = .01
 NICKEL = .05
 DIME = .10
 QUARTER = .25
-coffee_selection = ''
+COFFEE_SELECTION = ''
 
 MENU = {
     "espresso": {
@@ -54,7 +54,7 @@ def insert_coins():
 def give_change():
     '''Makes change based on how much money the user enters and how much
     coffee drink costs'''
-    change = f"{MONEY - MENU.get(coffee_selection).get('cost'):.2f}"
+    change = f"{MONEY - MENU.get(COFFEE_SELECTION).get('cost'):.2f}"
     return change
 
 
@@ -68,23 +68,23 @@ def report():
     print(f"Money: ${MONEY:.2f}")
 
 
-while coffee_selection != 'off':
-    coffee_selection = input("What would you like? (espresso/latte/cappuccino): ")
-    if coffee_selection == 'report':
+while COFFEE_SELECTION != 'off':
+    COFFEE_SELECTION = input("What would you like? (espresso/latte/cappuccino): ")
+    if COFFEE_SELECTION == 'report':
         report()
-    elif coffee_selection in MENU:
+    elif COFFEE_SELECTION in MENU:
         for key, value in resources.items():
-            if key in MENU.get(coffee_selection).get('ingredients'):
-                if resources[key] < MENU.get(coffee_selection).get('ingredients').get(key):
+            if key in MENU.get(COFFEE_SELECTION).get('ingredients'):
+                if resources[key] < MENU.get(COFFEE_SELECTION).get('ingredients').get(key):
                     print(f"Sorry there is not enough {key}")
                     break
         else:
             MONEY = float(insert_coins())
-            if MONEY > MENU.get(coffee_selection).get('cost'):
+            if MONEY > MENU.get(COFFEE_SELECTION).get('cost'):
                 change = give_change()
                 MONEY = 0.0
                 print(f"Here is ${change} dollars in change.")
-                for key in MENU.get(coffee_selection).get('ingredients'):
-                    resources[key] = resources[key] - MENU.get(coffee_selection).get('ingredients').get(key)
-                print(f"Here is your {coffee_selection}. Enjoy!")
+                for key in MENU.get(COFFEE_SELECTION).get('ingredients'):
+                    resources[key] = resources[key] - MENU.get(COFFEE_SELECTION).get('ingredients').get(key)
+                print(f"Here is your {COFFEE_SELECTION}. Enjoy!")
 sys.exit()
