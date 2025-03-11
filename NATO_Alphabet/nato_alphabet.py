f15573dea2343fa858ace96508bb12906eb01043
Ethan Lockwood <90814351+lockwood-ethan@users.noreply.github.com>
2025-03-10 21:47:12 -0400

    Implemented try/except blocks, load/update/dump json objects for Password Manager app

--- NATO_Alphabet/nato_alphabet.py
+++ NATO_Alphabet/nato_alphabet.py
@@ -4,6 +4,14 @@ nato_dataframe = pandas.read_csv("./NATO_Alphabet/nato_phonetic_alphabet.csv")
 
 nato_dict = {row.letter: row.code for (index, row) in nato_dataframe.iterrows()}
 
-word = input("Enter a word: ")
-nato_word = [nato_dict[letter.upper()] for letter in word]
-print(nato_word)
+def generate_phonetic():
+    word = input("Enter a word: ")
+    try:
+        nato_word = [nato_dict[letter.upper()] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(nato_word)
+
+generate_phonetic()
--- Password_Manager/password_manager.py
+++ Password_Manager/password_manager.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import string
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -16,16 +17,36 @@ def add_password():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
     
     if len(website) == 0 or len(password) == 0 or len(email) == 0:
         messagebox.showerror(title="Oops", message="Please don't leave any fields empty!")
-    else:    
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?")
-        if is_ok:
-            with open("Password_Manager\\data.txt", "a") as data:
-                data.write(f"{website} | {email} | {password}\n")
+    else:
+        try:
+            with open(".\\Password_Manager\\data.json", "r") as data_file:
+                #Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open(".\\Password_Manager\\data.json", "w") as data_file:
+                #Creating data.json file and adding new data
+                json.dump(new_data, data_file, indent=4)
+        else:
+            #Updating old data with new data
+            data.update(new_data)
+            with open(".\\Password_Manager\\data.json", "w") as data_file:
+                #Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             password_entry.delete(0, END)
             website_entry.delete(0, END)
+
+# ---------------------------- SEARCH ------------------------------- #
+#TODO: Create search function
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -40,9 +61,12 @@ canvas.grid(column=1, row=0)
 website_label = Label(text="Website:", bg="white")
 website_label.grid(column=0, row=1, pady=2)
 
-website_entry = Entry(width=52, relief="solid")
-website_entry.grid(column=1, columnspan=2, row=1, pady=2)
+website_entry = Entry(width=34, relief="solid")
+website_entry.grid(column=1, row=1, pady=2)
 website_entry.focus()
+
+search_button = Button(text="Search", width=14, relief="groove")
+search_button.grid(column=2, row=1, pady=2)
 
 email_label = Label(text="Email/Username:", bg="white")
 email_label.grid(column=0, row=2, pady=2)
