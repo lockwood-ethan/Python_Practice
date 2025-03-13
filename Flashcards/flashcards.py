b99461ea07ac1cd8a0e278230f481a1596c00eb6
Ethan Lockwood <90814351+lockwood-ethan@users.noreply.github.com>
2025-03-13 10:43:35 -0400

    Changed from csv to pandas library for reading the csv file, implemented card flip function

--- Flashcards/flashcards.py
+++ Flashcards/flashcards.py
@@ -1,33 +1,50 @@
 from tkinter import *
-from csv import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+data = pandas.read_csv(".\\Flashcards\\french_words.csv")
+to_learn = data.to_dict(orient="records")
+current_card = {}
+
 #----------------------- FUNCTIONS -----------------------#
-def get_new_word():
-    global row_num
-    with open('.\\Flashcards\\french_words.csv', newline='') as csvfile:
-        word_reader = reader(csvfile, delimiter=',')
-        # canvas.itemconfig(canvas_word_text, text=f"{wtf}")
-# TODO: Figure out how to increment by one word in the list when you press the button
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(canvas_image, image=card_front_img)
+    canvas.itemconfig(canvas_title_text, text="French", fill="black")
+    canvas.itemconfig(canvas_word_text, text=current_card["French"], fill="black")
+    flip_timer = window.after(3000, func=flip_card)
+    
+def flip_card():
+    canvas.itemconfig(canvas_image, image=card_back_img)
+    canvas.itemconfig(canvas_title_text, text="English", fill="white")
+    canvas.itemconfig(canvas_word_text, text=current_card["English"], fill="white")
+
 #----------------------- UI SETUP -----------------------#
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady= 50, bg=BACKGROUND_COLOR)
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(window, width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file=".\\Flashcards\\card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
-canvas_title_text = canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
-canvas_word_text = canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
+card_back_img = PhotoImage(file=".\\Flashcards\\card_back.png")
+canvas_image = canvas.create_image(400, 263, image=card_front_img)
+canvas_title_text = canvas.create_text(400, 150, text="", font=("Arial", 40, "italic"))
+canvas_word_text = canvas.create_text(400, 263, text="", font=("Arial", 60, "bold"))
 canvas.grid(column=0, columnspan=2, row=0)
 
 correct_img = PhotoImage(file=".\\Flashcards\\right.png")
-correct_button = Button(image=correct_img, highlightthickness=0, relief="flat", command=get_new_word)
+correct_button = Button(image=correct_img, highlightthickness=0, relief="flat", command=next_card)
 correct_button.grid(column=1, row=1)
 
 wrong_img = PhotoImage(file=".\\Flashcards\\wrong.png")
-wrong_button = Button(image=wrong_img, highlightthickness=0, relief="flat")
+wrong_button = Button(image=wrong_img, highlightthickness=0, relief="flat", command=next_card)
 wrong_button.grid(column=0, row=1)
+
+next_card()
 
 window.mainloop()
