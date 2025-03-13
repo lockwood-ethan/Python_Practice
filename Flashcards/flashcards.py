e4ffa675c1d5a9359698c2064879b51af4d3890e
Ethan Lockwood <90814351+lockwood-ethan@users.noreply.github.com>
2025-03-12 21:36:53 -0400

    Starting to build get new word function

--- Flashcards/flashcards.py
+++ Flashcards/flashcards.py
@@ -1,24 +1,29 @@
 from tkinter import *
+from csv import *
 
 BACKGROUND_COLOR = "#B1DDC6"
 
 #----------------------- FUNCTIONS -----------------------#
-
+def get_new_word():
+    global row_num
+    with open('.\\Flashcards\\french_words.csv', newline='') as csvfile:
+        word_reader = reader(csvfile, delimiter=',')
+        # canvas.itemconfig(canvas_word_text, text=f"{wtf}")
+# TODO: Figure out how to increment by one word in the list when you press the button
 #----------------------- UI SETUP -----------------------#
 window = Tk()
 window.title("Flashy")
-window.config(padx=100, pady= 50, bg=BACKGROUND_COLOR)
-window.geometry('1000x800')
+window.config(padx=50, pady= 50, bg=BACKGROUND_COLOR)
 
-canvas = Canvas(window, width=800, height=600, bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas = Canvas(window, width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file=".\\Flashcards\\card_front.png")
-canvas.create_image(400, 300, image=card_front_img)
-canvas_title_text = canvas.create_text(400, 175, text="Title", font=("Arial", 35, "italic"))
-canvas_word_text = canvas.create_text(400, 300, text="Word", font=("Arial", 50, "bold"))
+canvas.create_image(400, 263, image=card_front_img)
+canvas_title_text = canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
+canvas_word_text = canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
 canvas.grid(column=0, columnspan=2, row=0)
 
 correct_img = PhotoImage(file=".\\Flashcards\\right.png")
-correct_button = Button(image=correct_img, highlightthickness=0, relief="flat")
+correct_button = Button(image=correct_img, highlightthickness=0, relief="flat", command=get_new_word)
 correct_button.grid(column=1, row=1)
 
 wrong_img = PhotoImage(file=".\\Flashcards\\wrong.png")
